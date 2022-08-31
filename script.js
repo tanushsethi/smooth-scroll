454d254ca5c6f3b46dd3830e8a48cc4fd4121b41
Tanush Sethi <tanushsethi55@gmail.com>
2022-08-31 22:45:12 +0530

    final bug fixes

--- script.js
+++ script.js
@@ -3,17 +3,19 @@ function scroll(e){
     var section = document.getElementById(this.dataset.name);
     console.log(section);
     var scroller = setInterval(()=>{
-        if(section.getBoundingClientRect().top<=0){
+        console.log(section.getBoundingClientRect().top);
+        if(section.getBoundingClientRect().top<=1){
             clearInterval(scroller);
             return;
         }
-        window.scrollBy(0, 50);
-    }, 20);
+        window.scrollBy(0, 20);
+    }, 5);
 }
 
 var menuItems = document.querySelectorAll("nav a");
-
 for(var i = 0; i<menuItems.length ; i++){
     menuItems[i].addEventListener('click', scroll);
 }
+
+
 
